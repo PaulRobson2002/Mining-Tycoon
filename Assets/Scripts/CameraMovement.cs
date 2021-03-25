5989497cd4d50ac9a5771d0eb6eb390614d328a0
Paul <officialdrgaming@gmail.com>
2021-03-25 00:15:15 +0000

    Heatmap
    Pulled Codemonkeys version with heatmap for base

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,10 +1,21 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class CameraMovement : MonoBehaviour {
     // Update is called once per frame
-    void Update() {
+    private float zoomSpeed = 5;
+    private float targetOrtho;
+    private float smoothSpeed = 5.0f;
+    private float minOrtho = 2.0f;
+    private float maxOrtho = 80.0f;
+    void Start(){
+        targetOrtho = Camera.main.orthographicSize;
+    }
+
+    private void Update() {
         Vector3 moveDir = new Vector3(0, 0);
         if (Input.GetKey(KeyCode.W)) {
             moveDir.y = +1;
@@ -20,5 +31,14 @@ public class CameraMovement : MonoBehaviour {
         }
         float manualCameraSpeed = 80f;
         transform.position += moveDir * manualCameraSpeed * Time.deltaTime;
+
+
+        float scroll = Input.GetAxis ("Mouse ScrollWheel");
+        if (scroll != 0.0f) {
+             targetOrtho -= scroll * zoomSpeed;
+            targetOrtho = Mathf.Clamp (targetOrtho, minOrtho, maxOrtho);
+        }
+        
+         Camera.main.orthographicSize = Mathf.MoveTowards (Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
     }
 }
